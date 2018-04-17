0ac022f612d80e9fff009da47168dad1a1a55d31
Rin1827 <38449895+Rin1827@users.noreply.github.com>
2018-04-17 09:51:14 +0600

    Add files via upload

---
+++ labka.py
@@ -0,0 +1,54 @@
+import pygame
+def updatePoints(pts):
+    for i in range(45):
+        pts[0]-=1
+        pts[1]+=1
+        img = pygame.draw.circle(pygame.display.set_mode((800, 600)), color, pts, 100)
+        pygame.display.update()
+        pygame.display.flip()
+    for j in range(45):
+        pts[0]+=1
+        pts[1]+=1
+        img = pygame.draw.circle(pygame.display.set_mode((800, 600)), color, pts, 100)
+        pygame.display.update()
+        pygame.display.flip()
+    for k in range(45):
+        pts[0]+=1
+        pts[1]-=1
+        img = pygame.draw.circle(pygame.display.set_mode((800, 600)), color, pts, 100)
+        pygame.display.update()
+        pygame.display.flip()
+    for q in range(45):
+        pts[0]-=1
+        pts[1]-=1
+        img = pygame.draw.circle(pygame.display.set_mode((800, 600)), color, pts, 100)
+        pygame.display.update()
+        pygame.display.flip()
+
+def shear(pts):
+    for i in range(100):
+        pts[0] += 1
+        img = pygame.draw.circle(pygame.display.set_mode((800, 600)), color, pts, 100)
+        pygame.display.update()
+        pygame.display.flip()
+    for j in range(200):
+        pts[0] -= 1
+        img = pygame.draw.circle(pygame.display.set_mode((800, 600)), color, pts, 100)
+        pygame.display.update()
+        pygame.display.flip()
+    
+surface = pygame.display.set_mode((800, 600))
+color = (10, 100, 100)
+points = [200,150]
+pygame.display.init()
+img = pygame.draw.circle(surface, color, [200, 150], 50)
+pygame.display.flip()
+pygame.time.wait(1000)
+img = pygame.draw.circle(surface, color, [200, 150], 100)
+pygame.time.wait(1000)
+updatePoints([200,150])
+shear(points)
+pygame.display.flip()
+
+
+
